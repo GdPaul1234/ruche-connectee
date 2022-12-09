072f48dc452f8c8d6f6e3a3c210f20a9fdf83685
GdPaul1234 <paul.godin1234@outlook.fr>
2022-12-09 18:49:04 +0100

    Desktop left menu + hive icon

---
+++ frontend/src/components/icon.component.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  className?: string
+  rounded?: boolean
+  small?: boolean
+  logo: string
+}
+
+export default function IconComponent({ className, rounded, small, logo }: Props) {
+  const containerSize = () => small ? 'h-14 w-14' : 'h-44 w-48'
+  const logoSize = () => small ? 'h-16 w-16' : 'h-48 w-48'
+  const marginSize = () => small ? 'mt-[-12px]' : 'mt-[-24px]'
+  const roundedBorder = () => rounded ? 'rounded-md' : ''
+
+  return <div className={`${roundedBorder()} ${containerSize()} bg-amber-200 ${className}`}>
+    <img className={`block absolute ${logoSize()} ${marginSize()}`} src={logo} alt="Hive logo" />
+  </div>
+}
