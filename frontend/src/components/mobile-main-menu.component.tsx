2c102c05a884ff488a66f34de6f71efb215ffecb
GdPaul1234 <paul.godin1234@outlook.fr>
2023-01-06 18:10:51 +0100

    Mobile back button

--- frontend/src/components/mobile-main-menu.component.tsx
+++ frontend/src/components/mobile-main-menu.component.tsx
@@ -1,14 +1,25 @@
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import HiveListSelectorComponent, { Props } from "./hive-list-selector.component"
 
 import website_logo from '../ressources/logo192.png'
+import back_icon from '../ressources/back_icon.png'
 
 export default function MobileMainMenuComponent({ hives, className }: Props) {
   const location = useLocation()
+  const navigate = useNavigate()
+
+  function goBack() {
+    navigate(-1)
+  }
+
   const shouldShowHives = !location.pathname.includes('hives')
 
-  return <header className={`${className}`}>
-    <Link to='/' className="w-full">
+  return <header className={`relative ${className}`}>
+    <button className="absolute bottom-4 hover:opacity-60" onClick={goBack}>
+      <img src={back_icon} className="w-10 h-10" alt="go back" />
+    </button>
+
+    <Link to='/' className="w-full hover:opacity-60">
       <img src={website_logo} className="w-20 h-20 m-auto" alt="logo" />
     </Link>
 
