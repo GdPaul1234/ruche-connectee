c7f57e9c201522552dec79a04da85c213ac859a9
GdPaul1234 <paul.godin1234@outlook.fr>
2023-03-01 13:10:45 +0100

    WIP: simplify pusher

--- sensor_pusher/sensors/base_sensor_reader.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-from configparser import ConfigParser
-
-class BaseSensorReader(ABC):
-    def __init__(self, config: ConfigParser):
-        ...
-
-    def read(self):
-        ...
--- sensor_pusher/sensors/gps_reader.py
+++
@@ -1,9 +0,0 @@
-from sensor_pusher.sensors.base_sensor_reader import BaseSensorReader
-
-
-class GpsReader(BaseSensorReader):
-    def __init__(self):
-        pass
-
-    def read(self):
-        pass
