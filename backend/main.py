891bfd8b262ee49a637e3e628950e5967286dd8c
GdPaul1234 <paul.godin1234@outlook.fr>
2023-03-03 17:43:56 +0100

    Fix SPA routing

--- backend/main.py
+++ backend/main.py
@@ -16,15 +16,12 @@ from apps.user.token_router import router as token_router
 # source: https://stackoverflow.com/a/73552966
 
 class SPAStaticFiles(StaticFiles):
-
     async def get_response(self, path: str, scope):
         try:
             return await super().get_response(path, scope)
-        except HTTPException as ex:
-            if ex.status_code == 404:
-                return await super().get_response("index.html", scope)
-            else:
-                raise ex
+        except Exception:
+            return await super().get_response("index.html", scope)
+
 
 
 app = FastAPI()
@@ -51,7 +48,7 @@ app.mount("/", SPAStaticFiles(directory="../frontend/build", html=True), name="a
 
 
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000" # for dev env
 ]
 
 app.add_middleware(
